7c289de9d755e00111606d83b3e254db57735969
Bruno Abreu <bruno.abreu@redcloudtechnology.com>
2023-04-15 18:13:41 +0100

    Project Initial Project

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM apache/airflow:2.5.3-python3.8
+
+ENV AIRFLOW_HOME=/usr/local/airflow
+
+USER root
+
+#configs
+COPY config/entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+#COPY config/airflow.cfg ${AIRFLOW_HOME}/airflow.cfg
+
+#plugins
+COPY plugins ${AIRFLOW_HOME}/plugins
+
+#initial dags
+COPY dags /dags
+RUN mkdir ${AIRFLOW_HOME}/dags
+
+RUN chown -R airflow:airflow ${AIRFLOW_HOME}
+RUN chmod 777 -R /dags
+
+USER airflow
+
+#requirements
+COPY config/requirements.txt .
+RUN pip install --user --no-cache-dir -r requirements.txt
+
+
+EXPOSE 8080 5555 8793
+
+WORKDIR ${AIRFLOW_HOME}
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ config/entrypoint.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# copy files in EFS volume
+cp -R /dags/* /usr/local/airflow/dags/
+
+# start Airflow service as per
+#the previous parameter in command container
+case "$1" in
+  webserver)
+        airflow db init \
+        && airflow users create \
+        --role Admin \
+        --username airflow \
+        --password airflow \
+        --email airflow@example.com \
+        --firstname airflow \
+        --lastname airflow
+		sleep 5
+    exec airflow webserver
+    ;;
+  scheduler)
+    sleep 15
+    exec airflow "$@"
+    ;;
+  worker)
+    sleep 15
+    exec airflow celery "$@"
+    ;;
+  flower)
+    sleep 15
+    exec airflow celery "$@"
+    ;;
+  version)
+    exec airflow "$@"
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
